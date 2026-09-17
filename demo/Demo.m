6fb262b74f77dbeca11c0737522bbe82fad5a417
guxiaoyu <guxiaoyu@babeltime.com>
2026-09-17 19:32:40 +0800

    ci: Demo 编译失败也发 Release（附编译日志）便于定位

--- demo/Demo.m
+++ demo/Demo.m
@@ -253,9 +253,10 @@ willConnectToSession:(UISceneSession *)session
 
 @implementation IPATAppDelegate
 
+API_AVAILABLE(ios(13.0))
 - (UISceneConfiguration *)application:(UIApplication *)application
 configurationForConnectingSceneSession:(UISceneSession *)session
-                              options:(UISceneConnectionOptions *)options API_AVAILABLE(ios(13.0)) {
+                              options:(UISceneConnectionOptions *)options {
     UISceneConfiguration *cfg = [[UISceneConfiguration alloc] initWithName:@"Default Configuration"
                                                               sessionRole:session.role];
     cfg.delegateClass = [IPATSceneDelegate class];
